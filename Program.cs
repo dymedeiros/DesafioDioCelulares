a549424feb4d3975329a034719135975bf8f2555
Dyego Medeiros <dyegomedeiros@gmail.com>
2022-09-16 21:21:47 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+System.Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("999999","Lumia","1111",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone iPhone");
+Iphone iphone = new Iphone("7777","apple","222",128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegran");
